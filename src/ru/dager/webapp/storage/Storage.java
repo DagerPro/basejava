89581cf97fce802c6be4ec89b9286e76696482f3
Damir Kulanchakov <kdamir_ram@mail.ru>
2018-11-28 22:45:47 +0300

    HW2 hash code & equals & static methods

---
+++ src/ru/dager/webapp/storage/Storage.java
@@ -0,0 +1,27 @@
+package ru.dager.webapp.storage;
+
+import ru.dager.webapp.model.Resume;
+
+public interface Storage {
+
+    public void clear();
+
+    public void update(Resume r);
+
+    //сохранение резюме в массив
+    public void save(Resume r);
+
+    //получение резюме из массива
+    public Resume get(String uuid);
+
+    //удаление резюме из массива
+    public void delete(String uuid);
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll();
+
+    public int size();
+
+}
