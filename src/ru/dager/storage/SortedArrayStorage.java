a3127da2832a912fe8177e4eb01f9e1877f4a06b
Damir Kulanchakov <kdamir_ram@mail.ru>
2018-12-01 00:47:23 +0300

    HW3 save

--- src/ru/dager/storage/SortedArrayStorage.java
+++ src/ru/dager/storage/SortedArrayStorage.java
@@ -9,6 +9,14 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     public void save(Resume r) {
+        int newIndex = this.getIndex(r.getUuid());
+        if (newIndex < 0) storage[0] = r;
+        else {
+            for (int i = size; i < newIndex + 2; i--){
+                storage[i] = storage[i - 1];
+                storage[newIndex +1] = r;
+            }
+        }
 
     }
 
