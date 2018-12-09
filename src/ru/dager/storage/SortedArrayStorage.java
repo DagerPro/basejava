ba5ee657698ee32113466999e3fe9f8107fe6886
Damir Kulanchakov <kdamir_ram@mail.ru>
2018-12-09 22:48:28 +0300

    lesson 6 Inner class comporetor

--- src/ru/dager/storage/SortedArrayStorage.java
+++ src/ru/dager/storage/SortedArrayStorage.java
@@ -3,8 +3,26 @@ package ru.dager.storage;
 import ru.dager.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage{
+
+
+    /*
+    private static class ResumeComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    }
+*/
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
 
     @Override
     protected void fillDeletedElement(int index) {
@@ -26,6 +44,6 @@ public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
