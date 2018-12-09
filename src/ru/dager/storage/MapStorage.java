ae428bb672d044e8ed9bbf700ae1ef9fc865d0a8
Damir Kulanchakov <kdamir_ram@mail.ru>
2018-12-09 21:23:16 +0300

    HW5 MapStorage

---
+++ src/ru/dager/storage/MapStorage.java
@@ -0,0 +1,56 @@
+package ru.dager.storage;
+
+import ru.dager.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+
+    private Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return null;
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return false;
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return null;
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
