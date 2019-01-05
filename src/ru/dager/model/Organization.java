32d210255d3e3a13fb81661273395f108065e80b
Damir Kulanchakov <kdamir_ram@mail.ru>
2019-01-06 00:49:13 +0300

    L9 Stream

--- src/ru/dager/model/Organization.java
+++ src/ru/dager/model/Organization.java
@@ -2,6 +2,7 @@ package ru.dager.model;
 
 import ru.dager.util.DateUtil;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
@@ -12,7 +13,9 @@ import java.util.Objects;
 import static ru.dager.util.DateUtil.NOW;
 import static ru.dager.util.DateUtil.of;
 
-public class Organization {
+public class Organization implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final Link homePage;
     private List<Position> positions = new ArrayList<>();
 
@@ -44,7 +47,7 @@ public class Organization {
         return "Organization(" + homePage + "," + positions + ')';
     }
 
-    public static class Position {
+    public static class Position implements Serializable{
         private final LocalDate startDate;
         private final LocalDate endDate;
         private final String title;
